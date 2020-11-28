38f4844ae0e3741888b8b2b69468a6030e289117
NuooXu <xn19970505@gmail.com>
2020-11-28 00:03:52 -0500

    sliding window Q1

---
+++ SlidingWindow/MaxSumSubarraySizeK_E1/MaxSumSubarraySizeK_E.java
@@ -0,0 +1,39 @@
+package slidingwindow.MaxSumSubarraySizeK_E1;
+// Problem Statement 
+// Given an array of positive numbers and a positive number ‘k’, find the maximum sum of any contiguous subarray of size ‘k’.
+
+// Example 1:
+
+// Input: [2, 1, 5, 1, 3, 2], k=3 
+// Output: 9
+// Explanation: Subarray with maximum sum is [5, 1, 3].
+// Example 2:
+
+// Input: [2, 3, 4, 1, 5], k=2 
+// Output: 7
+// Explanation: Subarray with maximum sum is [3, 4].
+
+public class MaxSumSubarraySizeK_E {
+
+  public static int findMaxSumSubarray(int k, int[] arr){
+    int start = 0;
+    int sum = 0;
+    int max = 0;
+
+    for(int end = 0; end < arr.length; end++){
+      sum += arr[end];
+
+      if(end >= k - 1){
+        max = Math.max(max, sum);
+        sum -= arr[start];
+        start++;
+      }
+    }
+
+    return max;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Max sum of subarray of size K :" + findMaxSumSubarray(3, new int[] {2,1,5,1,3,2}));
+  }
+}
